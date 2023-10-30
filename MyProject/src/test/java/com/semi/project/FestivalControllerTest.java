aa3d7793fe3801ebb48d94c335e71d5a02134ae6
JunHyuk <kkjjhh374@naver.com>
2023-10-30 18:47:34 +0900

    src/test/java 생성

---
+++ MyProject/src/test/java/com/semi/project/FestivalControllerTest.java
@@ -0,0 +1,13 @@
+package com.semi.project;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/db-config.xml", "file:src/main/webapp/WEB-INF/spring/servlet-context.xml"})
+@WebAppConfiguration
+public class FestivalControllerTest {
+
+}
