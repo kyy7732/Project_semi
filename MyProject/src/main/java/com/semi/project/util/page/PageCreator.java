33f64068fb9e61a1f2e0f3effdacb2b4979735e8
JunHyuk <kkjjhh374@naver.com>
2023-11-08 22:10:58 +0900

    Paging 진행중

--- MyProject/src/main/java/com/semi/project/util/page/PageCreator.java
+++ MyProject/src/main/java/com/semi/project/util/page/PageCreator.java
@@ -2,6 +2,25 @@ package com.semi.project.util.page;
 
 public class PageCreator {
 
+	private static final int BUTTON_NUM = 5;
+	private int begin, end;
+	private boolean prev, next;
+	private Page page;
+	
+	private int articleTotalCount;
+	
+	public PageCreator(Page page, int articleTotalCount) {
+		this.page = page;
+		this.articleTotalCount = articleTotalCount;
+		calcDataOfPage();
+	}
+
+	private void calcDataOfPage() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 	
 	
 }
