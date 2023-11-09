d77910373bc6987c02a9fd754a52592407c3ac53
chaeyoon <codbs0703@naver.com>
2023-11-09 16:46:32 +0900

    오늘 중간 커밋!

--- MyProject/src/main/java/com/semi/project/festival/controller/FestivalController.java
+++ MyProject/src/main/java/com/semi/project/festival/controller/FestivalController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,12 +38,14 @@ public class FestivalController {
 		return service.getFestivalBySeason(season);
 	}
 	//행정구역 요청(비동기)
-	@GetMapping("/ftvList")
+	@PostMapping("/ftvList")
 	@ResponseBody
-	public List<FtvResponseDTO> getArea(@RequestBody String tag) {
-		log.info("/festival/ftvList 요청: GET!  {}", tag);
-		return service.getFestivalByArea(tag);
+	public List<FtvResponseDTO> getArea(@RequestBody String name) {
+		log.info("/festival/ftvList 요청: GET!  {}", name);
+		return service.getFestivalByArea(name);
 	}
+	
+	
 
 
 
